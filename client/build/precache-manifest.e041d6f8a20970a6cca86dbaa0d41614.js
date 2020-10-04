51a3aab2ce7df36273d5c2766407bad4be17df69
alexander austin howard <blazerv4@gmail.com>
2020-10-03 20:50:05 -0400

    build folder

---
+++ client/build/precache-manifest.e041d6f8a20970a6cca86dbaa0d41614.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efcde623b8577a113b939233c6b68d52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a0181b9cc51b0a6500c",
+    "url": "/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "509c3a179d8bc2ac0599",
+    "url": "/static/css/main.b4ca63bb.chunk.css"
+  },
+  {
+    "revision": "7a0181b9cc51b0a6500c",
+    "url": "/static/js/2.6b1ba87f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6b1ba87f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509c3a179d8bc2ac0599",
+    "url": "/static/js/main.71cdd42b.chunk.js"
+  },
+  {
+    "revision": "5c925270514860f04323",
+    "url": "/static/js/runtime-main.cf08d50b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
